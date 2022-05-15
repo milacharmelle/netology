46fb89d7c180d0bf1ed9671c09449004376182f3
Your Name <102684272+milacharmelle@users.noreply.github.com>
2022-05-15 21:46:38 +0500

    commit

--- src/main.java
+++ src/main.java
@@ -10,7 +10,7 @@ public class main {
 
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
-        System.out.println((potatoes / eaters) + " картофелин(а) ");
+        System.out.println((potatoes / eaters) + " картофелин(а)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
 }
